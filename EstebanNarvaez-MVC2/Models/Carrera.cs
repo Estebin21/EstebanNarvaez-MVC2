c43ab3cbfd907d4eb8294cfb1cc895e33137a457
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2024-10-21 18:35:22 -0500

    First change site.css

---
+++ EstebanNarvaez-MVC2/Models/Carrera.cs
@@ -0,0 +1,8 @@
+﻿namespace EstebanNarvaez_MVC2.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ EstebanNarvaez-MVC2/Models/EstudianteUDLA.cs
@@ -0,0 +1,10 @@
+﻿namespace EstebanNarvaez_MVC2.Models
+{
+    public class EstudianteUDLA
+    {
+        public String IdBanner { get; set; }
+
+        public String? Nombre { get; set; }
+        public Carrera carrera { get; set; }
+    }
+}

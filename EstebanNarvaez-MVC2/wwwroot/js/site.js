8ec1a7bb0ce775ce3ce429c0a1f98e180f73bfb5
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2024-10-21 20:08:53 -0500

    Primera parte site.css tercera semana

--- EstebanNarvaez-MVC2/wwwroot/js/site.js
+++ EstebanNarvaez-MVC2/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CrearAlerta() {
+    alert("Esta es una alerta");
+    var numero1 = document.getElementById("numero_1").value;
+    var numero2 = document.getElementById("numero_2").value;
+
+    var resultado = numero1 + numero2;
+    alert(resultado);
+
+}
